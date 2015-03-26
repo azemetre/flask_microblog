ff889447f2a29c7eb3f1702dbb13fe5c75ad55d1
Aaron Zemetres <azemetre@gmail.com>
2015-03-26 04:21:08 -0400

    Added custom error pages.

--- generator.py
+++ generator.py
@@ -127,6 +127,14 @@ def format_date(value, format='%B %d, %Y'):
 def index():
     return render_template('index.html', posts=blog.posts)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/blog/<path:path>/')
 def post(path):
     post = blog.get_post_or_404(path)
